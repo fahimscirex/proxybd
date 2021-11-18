69e5c603f00ccf70c3336062100862bf4580103c
fahimscirex <fahimscirex@tutanota.com>
2021-11-18 20:25:55 +0600

    Added anonymous proxy lists

---
+++ src/scraper.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+mkdir proxylist anonymous-proxylist && \
+curl https://raw.githubusercontent.com/monosans/proxy-list/main/proxies_geolocation/socks4.txt | grep Bangladesh | grep -Po '\d+.\d+.\d+.\d+[:]\d+' >> proxylist/socks4.txt
+curl https://raw.githubusercontent.com/monosans/proxy-list/main/proxies_geolocation/socks5.txt | grep Bangladesh | grep -Po '\d+.\d+.\d+.\d+[:]\d+' >> proxylist/socks5.txt
+curl https://raw.githubusercontent.com/monosans/proxy-list/main/proxies_geolocation/http.txt | grep Bangladesh | grep -Po '\d+.\d+.\d+.\d+[:]\d+' >> proxylist/http.txt
+curl https://raw.githubusercontent.com/monosans/proxy-list/main/proxies_geolocation_anonymous/socks4.txt | grep Bangladesh | grep -Po '\d+.\d+.\d+.\d+[:]\d+' >> anonymous-proxylist/socks4.txt
+curl https://raw.githubusercontent.com/monosans/proxy-list/main/proxies_geolocation_anonymous/socks5.txt | grep Bangladesh | grep -Po '\d+.\d+.\d+.\d+[:]\d+' >> anonymous-proxylist/socks5.txt
+curl https://raw.githubusercontent.com/monosans/proxy-list/main/proxies_geolocation_anonymous/http.txt | grep Bangladesh | grep -Po '\d+.\d+.\d+.\d+[:]\d+' >> anonymous-proxylist/http.txt
